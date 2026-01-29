ef0ccfa7d3b6214f583d5028bbbb8dcf43081778
杨凡 <90019795+ironkingdom2021@users.noreply.github.com>
2026-01-29 11:04:28 +0800

    add 8302 into config.py
    port number has to change to 8302 for dependency environment.

--- config.py
+++ config.py
@@ -10,7 +10,14 @@ def get_metric_baseurl(category_baseurl: str) -> str:
         return category_baseurl.replace('https://aitest.jsjfsz.com:8300', 'http://secondweb-service.agent-dns.svc.cluster.local:3000')
     if category_baseurl.startswith('https://aitest.jsjfsz.com:8300/agentdns/node3'):
         return category_baseurl.replace('https://aitest.jsjfsz.com:8300', 'http://thirdweb-service.agent-dns.svc.cluster.local:3000')
-    
+
+    if category_baseurl.startswith('https://ai.jsjfsz.com:8302/agentdns/node1'):
+        return category_baseurl.replace('https://ai.jsjfsz.com:8302', 'http://firstweb-service.agent-dns.svc.cluster.local:3000')
+    if category_baseurl.startswith('https://ai.jsjfsz.com:8302/agentdns/node2'):
+        return category_baseurl.replace('https://ai.jsjfsz.com:8302', 'http://secondweb-service.agent-dns.svc.cluster.local:3000')
+    if category_baseurl.startswith('https://ai.jsjfsz.com:8302/agentdns/node3'):
+        return category_baseurl.replace('https://ai.jsjfsz.com:8302', 'http://thirdweb-service.agent-dns.svc.cluster.local:3000')
+
     return category_baseurl
 
 def get_api_baseurl(category_baseurl: str) -> str:
@@ -21,6 +28,13 @@ def get_api_baseurl(category_baseurl: str) -> str:
         api_baseurl = category_baseurl.replace('https://aitest.jsjfsz.com:8300', 'http://secondweb-service.agent-dns.svc.cluster.local:3000')
     if category_baseurl.startswith('https://aitest.jsjfsz.com:8300/agentdns/node3'):
         api_baseurl = category_baseurl.replace('https://aitest.jsjfsz.com:8300', 'http://thirdweb-service.agent-dns.svc.cluster.local:3000')
+
+    if category_baseurl.startswith('https://ai.jsjfsz.com:8302/agentdns/node1'):
+        api_baseurl = category_baseurl.replace('https://ai.jsjfsz.com:8302', 'http://firstweb-service.agent-dns.svc.cluster.local:3000')
+    if category_baseurl.startswith('https://ai.jsjfsz.com:8302/agentdns/node2'):
+        api_baseurl = category_baseurl.replace('https://ai.jsjfsz.com:8302', 'http://secondweb-service.agent-dns.svc.cluster.local:3000')
+    if category_baseurl.startswith('https://ai.jsjfsz.com:8302/agentdns/node3'):
+        api_baseurl = category_baseurl.replace('https://ai.jsjfsz.com:8302', 'http://thirdweb-service.agent-dns.svc.cluster.local:3000')
 
     if api_baseurl.endswith('/mcp/$smart'):
         api_baseurl = api_baseurl.replace('/mcp/$smart', '')
